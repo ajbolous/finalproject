4cb0f19bcf316013ab7d58d052067e492c83bd3b
Bolous AbuJaber <ajbolous@gmail.com>
2017-04-15 08:50:30 +0300

    style

--- server/machines/models.py
+++ server/machines/models.py
@@ -2,10 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.forms.models import model_to_dict
-<<<<<<< HEAD
 
-=======
->>>>>>> 44184c3aacb3ad5bbe8594a0ef55bf50b9042bf4
 # Create your models here.
 class Machine(models.Model):
     mModel = models.CharField(max_length=50)
@@ -21,13 +18,6 @@ class Machine(models.Model):
     mLat = models.FloatField()
     mIsAvailable = models.BooleanField()
     mFuelLevel = models.FloatField()
-<<<<<<< HEAD
 
     def toJSON(self):
         return model_to_dict(self)
-=======
-    
-    def toJSON(self):
-        return model_to_dict(self)
-
->>>>>>> 44184c3aacb3ad5bbe8594a0ef55bf50b9042bf4
--- server/machines/views.py
+++ server/machines/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-# Create your views here.
-<<<<<<< HEAD
-def getAllMachines(request):
-    pass
-
-def getMachine(request):
-    pass
-
-def addMachine(request):
-    pass
-
-def deleteMachine(request):
-    pass
-
-def editMachine(request):
-    pass
-=======
 from machines.models import Machine     
+
+# Create your views here.
 
 def getMachines(request):
     machines  = Machine.objects.all()
     machinesJson = [m.toJSON() for m in machines]
     return JsonResponse(machinesJson, safe=False)
->>>>>>> 44184c3aacb3ad5bbe8594a0ef55bf50b9042bf4
