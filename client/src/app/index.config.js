a9993b500c114224929bce99ee74fcc8a4fa827e
Bolous AbuJaber <ajbolous@gmail.com>
2017-04-23 22:58:38 +0300

    deleted unused config files

--- client/src/app/index.config.js
+++ client/src/app/index.config.js
@@ -1,12 +1,17 @@
+var DJANGOURL = "http://localhost:8000";
+
 (function() {
     'use strict';
 
     angular
         .module('opmopApp')
-        .config(config);
+        .config(config)
+        .constant('malarkey', malarkey)
+        .constant('toastr', toastr)
+        .constant('moment', moment);
 
     /** @ngInject */
-    function config($logProvider, toastr) {
+    function config($logProvider, toastr, $routeProvider, $locationProvider) {
         // Enable log
         $logProvider.debugEnabled(true);
 
@@ -15,6 +20,36 @@
         toastr.options.positionClass = 'toast-top-right';
         toastr.options.preventDuplicates = true;
         toastr.options.progressBar = true;
+
+        // Set Router
+        $locationProvider.html5Mode(true);
+        $locationProvider.hashPrefix('#');
+
+        //ROUTES 
+        $routeProvider.when('/', {
+            templateUrl: 'app/views/main/main.html',
+            controller: 'MainController',
+            controllerAs: '$ctrl'
+        });
+
+        $routeProvider.when('/machines', {
+            templateUrl: 'app/views/machines/machines.html',
+            controller: 'MachinesController',
+            controllerAs: '$ctrl'
+        });
+
+        $routeProvider.when('/tasks', {
+            templateUrl: 'app/views/tasks/tasks.html',
+            controller: 'TasksController',
+            controllerAs: '$ctrl'
+        });
+
+        $routeProvider.when('/maps', {
+            templateUrl: 'app/views/maps/maps.html',
+            controller: 'MapsController',
+            controllerAs: '$ctrl'
+        });
+
     }
 
 })();
