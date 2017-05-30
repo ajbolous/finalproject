876a620cdcac521b51efcd6b6adb893e5251233b
Bolous AbuJaber <ajbolous@gmail.com>
2017-05-30 19:22:12 +0300

    added new scripts

--- client/src/app/components/navbar/navbar.component.js
+++ client/src/app/components/navbar/navbar.component.js
@@ -11,25 +11,43 @@
         var $ctrl = this;
         $ctrl.navbarCollapsed = true;
 
-        $ctrl.links = [
-            { label: 'Monitor', path: '/', active: true },
-            { label: 'Maps', path: '/maps', active: false },
-            { label: 'Machines', path: '/machines', active: false },
-            { label: 'Tasks', path: '/tasks', active: false },
-            { label: 'Admin', path: '/admin', active: false },
-            { label: 'Help', path: '/help', active: false },
+        $ctrl.leftLinks = [
+            { label: 'Monitor', path: '/', active: true, icon: "fa-desktop" },
+            { label: 'Maps', path: '/maps', active: false, icon: "fa-map-marker" },
+            { label: 'Machines', path: '/machines', active: false, icon: "fa-truck" },
+            { label: 'Tasks', path: '/tasks', active: false, icon: "fa-tasks" },
         ]
 
-        $ctrl.links.forEach(function(link) {
+        $ctrl.rightLinks = [
+
+            { label: 'Admin', path: '/admin', active: false, icon: "fa-user" },
+            { label: 'Help', path: '/help', active: false, icon: "fa-asterisk" },
+        ]
+
+        $ctrl.leftLinks.forEach(function(link) {
             link.active = false;
 
             if ($location.path().endsWith(link.path)) {
                 link.active = true;
             };
         });
+
+        $ctrl.rightLinks.forEach(function(link) {
+            link.active = false;
+
+            if ($location.path().endsWith(link.path)) {
+                link.active = true;
+            };
+        });
+
+
         $ctrl.selectLink = function(link) {
             $log.debug(link)
-            this.links.forEach(function(l) {
+            this.leftLinks.forEach(function(l) {
+                l.active = false;
+            });
+
+            this.rightLinks.forEach(function(l) {
                 l.active = false;
             });
             link.active = true;
